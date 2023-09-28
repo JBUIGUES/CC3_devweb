87f40cd5ed9ad29b3dd750892fe7edd6645f6e57
JBUIGUES <79887011+JBUIGUES@users.noreply.github.com>
2023-09-28 19:39:38 +1100

    Update server-http.mjs

--- CC3/devweb-tp5/server-http.mjs
+++ CC3/devweb-tp5/server-http.mjs
@@ -4,18 +4,36 @@ import fs from "node:fs/promises";
 const host = "localhost";
 const port = 8000;
 
-async function requestListener(_request, response) {
+async function requestListener(request, response) {
+  response.setHeader("Content-Type", "text/html");
   try {
-    const contents = await fs.readFile("devweb-tp5/index.html", "utf8");
-    response.setHeader("Content-Type", "text/html");
-    response.writeHead(200);
-    response.end(contents);
+    const urlParts = request.url.split("/");
+
+    if (urlParts[1] === "" || urlParts[1] === "index.html") {
+      const contents = await fs.readFile("devweb-tp5/index.html", "utf8");
+      response.writeHead(200);
+      return response.end(contents);
+    }
+
+    if (urlParts[1] === "random" && urlParts[2] && !isNaN(urlParts[2])) {
+      const nb = parseInt(urlParts[2]);
+      const randomNumbers = [];
+      for (let i = 0; i < nb; i++) {
+        randomNumbers.push(Math.floor(100 * Math.random()));
+      }
+      response.writeHead(200);
+      return response.end(`<html><p>${randomNumbers.join(", ")}</p></html>`);
+    }
+
+    response.writeHead(404);
+    return response.end(`<html><p>404: NOT FOUND</p></html>`);
   } catch (error) {
     console.error(error);
-    response.writeHead(500, { "Content-Type": "text/plain" });
-    response.end("Internal Server Error");
+    response.writeHead(500);
+    return response.end(`<html><p>500: INTERNAL SERVER ERROR</p></html>`);
   }
 }
+
 
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
